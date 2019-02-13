57d4f00d1ab6a6c7cf73c2640a83037cfbc4ef77
Alex Merecka <merecka@gmail.com>
2019-02-13 21:08:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ def reverse_each_word(sentence)
   end
   reverse_sentence.join(" ")
   
-  original_sentence.collect {|x| x.reverse!}
-  original_sentnece.join(" ")
+  ## original_sentence.collect {|x| x.reverse!}
+  ## original_sentnece.join(" ")
     
 end
